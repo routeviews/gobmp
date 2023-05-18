7efb5a948978f2abbb12524ff6fd20bc5108c1d2
Anton Berezin <tobez@tobez.org>
2023-05-18 16:32:50 +0000

    Calculate topic from topic type, config, and vars.

--- pkg/topic/topic.go
+++ pkg/topic/topic.go
@@ -1,10 +1,11 @@
 // Package topic computes the names of Kafka topics to send processed data to.
-// The names depend on the message type, router IP address, and XXX,
-// and can be configured via a configuration file.
+// The names depend on the message type, router IP address, peer IP address,
+// and peer ASN, and can be configured via a configuration file.
 // The configuration file will be automatically re-read upon receiving SIGHUP.
 package topic
 
 import (
+	"net/netip"
 	"strings"
 )
 
@@ -26,9 +27,30 @@ func Cleanup() {
 	config.bye <- 1
 }
 
-func substitute(template string, subs map[string]string) string {
-	for key, value := range subs {
-		template = strings.Replace(template, "{{"+key+"}}", value, -1)
+// GetTopic computes a destination topic from the topic type t and a map of variable substitutions vars.
+// If the topic template for the specified topic type is not found in the configuration,
+// an empty string is returned.
+// A missing template variable is substituted with an empty string.
+func GetTopic(t string, vars map[string]string) string {
+	config.mu.Lock()
+	templ := config.topicTemplate[t]
+	config.mu.Unlock()
+	if templ == "" {
+		return ""
 	}
-	return template
+
+	routerIP := vars["router_ip"]
+	if routerIP == "" {
+		vars["named_router"] = ""
+	} else {
+		vars["named_router"], _ = config.GetNamedRouter(netip.MustParseAddr(routerIP))
+	}
+
+	res := templ
+	for key, value := range vars {
+		res = strings.Replace(res, "{{"+key+"}}", value, -1)
+	}
+
+	// glog.V(5).Infof("template(%s)=%#v, vars %#v, result: %s", t, templ, vars, res)
+	return res
 }

71a5d0ee249e3946a054fbf45004279fc50cdc1e
Anton Berezin <tobez@tobez.org>
2023-05-18 16:30:43 +0000

    Remove topic debug prints.

--- pkg/topic/debug.go
+++ pkg/topic/debug.go
@@ -6,16 +6,20 @@ import (
 	"github.com/golang/glog"
 )
 
-func Debug() {
-	for _, ip := range []string{"203.159.70.49", "1.2.3.5", "fed0::400"} {
-		s, err := config.GetNamedRouter(netip.MustParseAddr(ip))
-		glog.V(5).Infof("ip: %s, group: %s, err: %+v", ip, s, err)
-	}
+// In case we need those imports.
+var _ = netip.MustParseAddr
+var _ = glog.Infof
 
-	subs := map[string]string{}
-	ip := "128.223.51.23"
-	s, _ := config.GetNamedRouter(netip.MustParseAddr(ip))
-	subs["named_router"] = s
-	glog.V(5).Infof("rawTemplate: %s, ip: %s, result: %s", config.rawTemplate, ip,
-		substitute(config.rawTemplate, subs))
+func Debug() {
+	// for _, ip := range []string{"203.159.70.49", "1.2.3.5", "fed0::400"} {
+	// 	s, err := config.GetNamedRouter(netip.MustParseAddr(ip))
+	// 	glog.V(5).Infof("ip: %s, group: %s, err: %+v", ip, s, err)
+	// }
+
+	// subs := map[string]string{}
+	// ip := "128.223.51.23"
+	// s, _ := config.GetNamedRouter(netip.MustParseAddr(ip))
+	// subs["named_router"] = s
+	// glog.V(5).Infof("rawTemplate: %s, ip: %s, result: %s", config.topicTemplate["raw"], ip,
+	// 	substitute(config.topicTemplate["raw"], subs))
 }
